f8eb32be3df5f30782211d1dfbfcf35fb35500df
LIZETHVERA <lizeth.veraro@gmail.com>
2019-08-15 15:13:15 -0700

    Heroku mongo db

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const path = require("path");
 const mongoose = require("mongoose");
-
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -13,19 +12,19 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-
 
 // API routes
 require("./routes/apiRoutes")(app);
 
-// Send every request to the React app
+// Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
